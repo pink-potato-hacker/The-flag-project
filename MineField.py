bd7b394d1712ef3f0cfccc68c69d2752a7a479ab
Вероника Гришина <grishinaveronika00@gmail.com>
2022-09-14 15:23:49 +0300

    Created 2 surfaces - dark and light. Changed the Consts

--- MineField.py
+++ MineField.py
@@ -4,12 +4,10 @@ import random
 mine_field = []
 mines = [[] for n in range(20)]
 
-
 # change for loop
 def create_empty_mine_field():
     global mine_field
     mine_field = [[Consts.EMPTY for col in range(Consts.NUMBER_OF_COLUMNS)] for row in range(Consts.NUMBER_OF_ROWS)]
-
 
 def randomize_mines():
 
@@ -21,7 +19,6 @@ def randomize_mines():
         for i in range(3):
             mine_field[rnd_row][rnd_col + i] = Consts.MINES
             mines[mine_index].append(rnd_col + i)
-
 
 def put_flag():
     pass
