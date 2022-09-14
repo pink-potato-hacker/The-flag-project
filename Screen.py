1f83231dc4a83ee649d52fc29483d45f18e5569b
pink-potao <lihi.zachis1223@gmail.com>
2022-09-14 11:43:07 +0300

    modules

--- Screen.py
+++ Screen.py
@@ -3,29 +3,43 @@ import pygame
 import pygame
 import Consts
 
-def fill_colors(matrix, screen):
-    # run over all the cells in the current matrix
-    for i in range(Consts.NUMBER_OF_ROWS):
-        for j in range(Consts.NUMBER_OF_COLUMNS):
-            color = Consts.BACKGROUND_COLOR
-            pygame.draw.rect(screen, color, (j * Consts.SIZE, i * Consts.SIZE, Consts.SIZE - 1, Consts.SIZE - 1))
-    pygame.display.update()
-def create_board():
-    pygame.init()
-    # Set the screen size
-    screen = pygame.display.set_mode((Consts.NUMBER_OF_COLUMNS * Consts.SIZE, Consts.NUMBER_OF_ROWS * Consts.SIZE))
+pygame.init()
+
+# Initializing surface
+surface = pygame.display.set_mode((500, 500))
+
+# Initialing RGB Color
+color = (190, 229, 176)
+
+# Changing surface color
+while True:
+    surface.fill(color)
     pygame.display.flip()
-    pygame.display.update()
-    # Create zeros matrix
-    matrix = [[0 for x in range(Consts.NUMBER_OF_COLUMNS)] for y in range(Consts.NUMBER_OF_ROWS)]
-    fill_colors(matrix, screen)
 
-    while True:
-        # Wait for events
-        for event in pygame.event.get():
-            # if user wants to QUIT, close pygame
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-create_board()
+#
+# def fill_colors(matrix, screen):
+#     # run over all the cells in the current matrix
+#     for i in range(Consts.NUMBER_OF_ROWS):
+#         for j in range(Consts.NUMBER_OF_COLUMNS):
+#             color = Consts.BACKGROUND_COLOR
+#             pygame.draw.rect(screen, color, (j * Consts.SIZE, i * Consts.SIZE, Consts.SIZE - 1, Consts.SIZE - 1))
+#     pygame.display.update()
+# def create_board():
+#     pygame.init()
+#     # Set the screen size
+#     screen = pygame.display.set_mode((Consts.NUMBER_OF_COLUMNS * Consts.SIZE, Consts.NUMBER_OF_ROWS * Consts.SIZE))
+#     pygame.display.flip()
+#     pygame.display.update()
+#     # Create zeros matrix
+#     matrix = [[0 for x in range(Consts.NUMBER_OF_COLUMNS)] for y in range(Consts.NUMBER_OF_ROWS)]
+#     fill_colors(matrix, screen)
+#
+#     while True:
+#         # Wait for events
+#         for event in pygame.event.get():
+#             # if user wants to QUIT, close pygame
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 return
+#
+# create_board()
