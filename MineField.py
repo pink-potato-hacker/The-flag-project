bf40e1de8d0c53637acc990c400cbf6e3b28f70f
shir <mikoxtragi@gmail.com>
2022-09-14 15:17:13 +0300

    create mine filed matrix with random mines, fix consts

--- MineField.py
+++ MineField.py
@@ -5,15 +5,15 @@ mine_field = []
 mines = [[] for n in range(20)]
 
 
+# change for loop
 def create_empty_mine_field():
     global mine_field
     mine_field = [[Consts.EMPTY for col in range(Consts.NUMBER_OF_COLUMNS)] for row in range(Consts.NUMBER_OF_ROWS)]
 
 
 def randomize_mines():
-    mine_index = 0
 
-    for rand in range(20):
+    for mine_index in range(20):
         rnd_row = random.randint(0, 24)
         rnd_col = random.randint(0, 46)
         mines[mine_index].append(rnd_row)
@@ -21,7 +21,7 @@ def randomize_mines():
         for i in range(3):
             mine_field[rnd_row][rnd_col + i] = Consts.MINES
             mines[mine_index].append(rnd_col + i)
-        mine_index += 1
+
 
 def put_flag():
     pass
