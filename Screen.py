d0b0c56928c3d76091982c14cdf26f89ad27a2aa
shir <mikoxtragi@gmail.com>
2022-09-14 11:26:45 +0300

    adding more const

--- Screen.py
+++ Screen.py
@@ -1,6 +1,7 @@
 import pygame
 import Consts
 
+
 def fill_colors(matrix, screen):
     # run over all the cells in the current matrix
     for i in range(Consts.NUMBER_OF_ROWS):
@@ -8,6 +9,8 @@ def fill_colors(matrix, screen):
             color = Consts.BACKGROUND_COLOR
             pygame.draw.rect(screen, color, (j * Consts.SIZE, i * Consts.SIZE, Consts.SIZE - 1, Consts.SIZE - 1))
     pygame.display.update()
+
+
 def create_board():
     pygame.init()
     # Set the screen size
@@ -25,5 +28,6 @@ def create_board():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
 
 create_board()
