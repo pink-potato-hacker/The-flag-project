686069084b3e61a27ed787005ab3964b3d828202
pink-potao <lihi.zachis1223@gmail.com>
2022-09-15 10:52:06 +0300

    never mind

--- Flag.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import Consts
-import Screen
-
-
-def placing_flag():
-    flag_img = pygame.image.load("png files/flag.png").convert_alpha()
-    flag_img = pygame.transform.scale(flag_img, (Consts.SIZE * 2, Consts.SIZE * 4))
-    Screen.screen.blit(flag_img, (1200, 525))
