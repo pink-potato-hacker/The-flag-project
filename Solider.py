0de92f68ce97082bcfaaa0055b004550f2182cb4
lihi <113342566+pink-potato-hacker@users.noreply.github.com>
2022-09-15 09:31:16 +0300

    Update Solider.py

--- Solider.py
+++ Solider.py
@@ -3,6 +3,19 @@ import Screen
 import pygame
 
 def placing_soldier(coords_tuple):
-    soldier_img = pygame.image.load("png files/soldier.png").convert()
+    soldier_img = pygame.image.load("png files/soldier.png").convert_alpha()
     soldier_img = pygame.transform.scale(soldier_img, (Consts.SIZE * 2, Consts.SIZE * 4))
-    screen.blit(soldier_img, coords_tuple)
+    Screen.screen.blit(soldier_img, coords_tuple)
+
+def moving_soldier(soldier_x_location, soldier_y_location):
+    while True:
+        key_input = pygame.key.get_pressed()
+        if key_input[pygame.K_LEFT]:
+            soldier_x_location -= 10
+        if key_input[pygame.K_UP]:
+            soldier_y_location -= 10
+        if key_input[pygame.K_RIGHT]:
+            soldier_x_location += 10
+        if key_input[pygame.K_DOWN]:
+            soldier_y_location += 10
+        return ((soldier_x_location, soldier_y_location))
