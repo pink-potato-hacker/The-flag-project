67bb3949d9708d9169aee2d6cc9af623aa8fe73f
shir <mikoxtragi@gmail.com>
2022-09-15 09:52:42 +0300

    boom no flag boom

--- MineField.py
+++ MineField.py
@@ -5,6 +5,7 @@ mine_field = []
 mines = [[] for n in range(20)]
 grass = []
 
+
 def create_empty_mine_field():
     global mine_field
 
@@ -14,10 +15,16 @@ def create_empty_mine_field():
             row.append(Consts.EMPTY)
         mine_field.append(row)
 
+
 def randomize_mines():
     for mine_index in range(20):
         rnd_row = random.randint(0, 24)
         rnd_col = random.randint(0, 46)
+
+        while (rnd_row <= 3 and rnd_col <= 2) or (rnd_row >= 22 and rnd_col >= 45):
+            rnd_row = random.randint(0, 24)
+            rnd_col = random.randint(0, 46)
+
         mines[mine_index].append(rnd_row)
 
         for i in range(3):
@@ -31,10 +38,12 @@ def randomize_grass():
         rnd_y = random.randint(0, Consts.NUMBER_OF_ROWS * 25)
         grass.append((rnd_x, rnd_y))
 
+
 def put_flag():
     for row in range(21, 24):
         for col in (46, 50):
             mine_field[row][col] = Consts.FLAG
+
 
 create_empty_mine_field()
 randomize_mines()
