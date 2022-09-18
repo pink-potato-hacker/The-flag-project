86584331654ecedfa623be6ab84945da44edb27e
shir <mikoxtragi@gmail.com>
2022-09-18 10:58:40 +0300

    bad bad practise function

--- Database.py
+++ Database.py
@@ -1,30 +1,76 @@
 import pandas
 import pygame
-import sys
 
 
-def saving():
+def key_pressed():
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            timer = pygame.time.get_ticks()
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    return 1
+                    if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                        return 1.5
+                    else:
+                        return 1
+
                 if event.key == pygame.K_2:
-                    return 2
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 2.5
+                            else:
+                                return 2
+
                 if event.key == pygame.K_3:
-                    return 3
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 3.5
+                            else:
+                                return 3
                 if event.key == pygame.K_4:
-                    return 4
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 4.5
+                            else:
+                                return 4
                 if event.key == pygame.K_5:
-                    return 5
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 5.5
+                            else:
+                                return 5
                 if event.key == pygame.K_6:
-                    return 6
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 6.5
+                            else:
+                                return 6
+
                 if event.key == pygame.K_7:
-                    return 7
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 7.5
+                            else:
+                                return 7
+
                 if event.key == pygame.K_8:
-                    return 8
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 8.5
+                            else:
+                                return 8
+
                 if event.key == pygame.K_9:
-                    return 9
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            if (pygame.time.get_ticks() - timer) / 1000 <= 1:
+                                return 9.5
+                            else:
+                                return 9
